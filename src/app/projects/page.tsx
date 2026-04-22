e6af0714913a6580a09ef38bd23a2cd611b41507
Rodrigo Augusto Vieira <rodrigoaugustov@gmail.com>
2026-04-21 22:40:14 -0300

    Force dynamic rendering for projects list page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import { projectService } from '@/server/persistence/project-service'
 import { ProjectPicker } from '@/app/projects/project-picker'
 
+export const dynamic = 'force-dynamic'
+
 export default async function ProjectsPage() {
   const projects = await projectService.listProjects()
 
